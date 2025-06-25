e365335e4fde84acfd60895057b56b1884e84eb1
Ayush <w.ayush@iitg.ac.in>
2025-06-25 06:02:00 +0530

    La la la lala

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-})
+	plugins: [react(), tailwindcss()],
+	base: process.env.VITE_BASE_PATH || '/InCite-AI',
+	server: {
+		proxy: {
+			'/api': {
+				target: process.env.BACKEND_URL || 'http://localhost:5000',
+				changeOrigin: true,
+				secure: false,
+			},
+		},
+	},
+});
